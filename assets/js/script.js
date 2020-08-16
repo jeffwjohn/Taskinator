ca0728481959af68d8081af1bf55d03b560dbb1f
Jeff Johnston <jeffwjohn@yahoo.com>
2020-08-16 16:52:29 -0500

    fix bug

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 var tasksInProgressEl = document.querySelector("#tasks-in-progress");
 var tasksCompletedEl = document.querySelector("#tasks-completed");
-var pageContentEL = document.querySelector("#page-content");
+var pageContentEl = document.querySelector("#page-content");
 var taskIdCounter = 0;
 var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
@@ -162,11 +162,22 @@ var deleteTask = function (taskId) {
 };
 
 var taskStatusChangeHandler = function (event) {
+    // get the task item's id
+    var taskId = event.target.getAttribute("data-task-id");
 
+    // get the currently selected option's value and convert to lowercase
+    var statusValue = event.target.value.toLowerCase();
 
-    console.log(event.target);
-    console.log(event.target.getAttribute("data-task-id"));
+    // find the parent task item element based on the id
+    var taskSelected = document.querySelector(".task-item[data-task-id='" + taskId + "']");
+    if (statusValue === "to do") {
+        tasksToDoEl.appendChild(taskSelected);
+    } else if (statusValue === "in progress") {
+        tasksInProgressEl.appendChild(taskSelected);
+    } else if (statusValue === "completed") {
+        tasksCompletedEl.appendChild(taskSelected);
+    }
 };
 
 pageContentEl.addEventListener("change", taskStatusChangeHandler);
-pageContentEL.addEventListener("click", taskButtonHandler);
+pageContentEl.addEventListener("click", taskButtonHandler);
